56a76007e1a9509df89532a82b64999841a64526
Drobilica <dusan@drobilica.com>
2023-12-05 12:56:11 +0100

    Add clock and taskbar to the page

--- cde/script.js
+++ cde/script.js
@@ -5,3 +5,22 @@ document.querySelectorAll('.desktop-icon').forEach(icon => {
         window.open(icon.href, '_blank');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    function updateClock() {
+        var now = new Date();
+        var hours = now.getHours();
+        var minutes = now.getMinutes();
+        var ampm = hours >= 12 ? 'PM' : 'AM';
+
+        hours = hours % 12;
+        hours = hours ? hours : 12; // Hour '0' should be '12'
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+
+        var timeString = hours + ':' + minutes + ' ' + ampm;
+        document.getElementById('clock').innerHTML = timeString;
+    }
+
+    setInterval(updateClock, 10000);
+    updateClock(); // Initial call to display clock immediately
+});
