58d5f4f0850874819153b6e607ee9eac76eaff20
drobilica <dusan@drobilica.com>
2025-03-11 02:55:50 +0100

    Updated landing page ignore files

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,4 +5,22 @@ export default {
     plugins: [
         tailwindcss(),
       ],
+    server: {
+      // These headers are applied during development only
+      headers: {
+        'Content-Security-Policy': "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; img-src 'self'; frame-ancestors 'none';",
+        'X-Frame-Options': 'DENY',
+        'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+      }
+    },
+    content: [
+      '!./sites',
+      '!./**/node_modules',
+      "./sites/**/*.{html,js}",
+      "!./sites/**/node_modules/**/*.{html,js}" // <- Add
+
+
+
+    ],
 }
+
